40acb57bc6f1389ba115eed70e1b26ae3c5d4984
Grunde Lovoll <grunde.lovoll@basefarm.com>
2021-04-21 09:30:54 +0200

    added minimal app and requirements file

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Flycast simulator")
+st.write("""
+This is the start of an open and free flycasting simulator.
+
+The app is a work in process and it will be developed in an ad-hock
+fashion when I have the time.
+The project will also be used to learn and get practical experience
+using the Streamlit framework.
+""")
